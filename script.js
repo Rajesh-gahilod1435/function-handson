907ac90bc027c03dd4fcc735cb87c9b02857f6d5
Rajesh-gahilod1435 <120488895+Rajesh-gahilod1435@users.noreply.github.com>
2023-06-08 15:56:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// "Guess the Output
+
+function FindSum(a, b){
+    return a + b;
+}
+
+function DisplayData(data, batch){
+    console.log(`i am from ${data} and My batch is EA${batch}`)
+}
+DisplayData("PrepBytes", FindSum(10, 9));
